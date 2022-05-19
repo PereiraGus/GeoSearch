f021f0f8d0f46fcbe44fe0f2128a21ec87fca536
PereiraGus <gustavo.oito@hotmail.com>
2022-05-19 10:35:22 -0300

    Mais atualizações da tela de menu

---
+++ app/src/main/java/com/example/geosearch/MainMenu.java
@@ -0,0 +1,14 @@
+package com.example.geosearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
