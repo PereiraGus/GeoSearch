e0687c3c084fcf46934579f4b925fb8d29f129f5
Gustavo Pereira <gustavo.oito@hotmail.com>
2022-05-21 12:40:12 -0300

    Mudanças na tela principal, adição da tela de perfil e barra de menu

--- app/src/main/java/com/example/geosearch/MainMenu.java
+++ app/src/main/java/com/example/geosearch/MainMenu.java
@@ -9,6 +9,6 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_menu);
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/example/geosearch/profileActivity.java
+++ app/src/main/java/com/example/geosearch/profileActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class test extends AppCompatActivity {
+public class profileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_profile);
     }
 }
