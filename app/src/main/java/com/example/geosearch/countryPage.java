d1207fbb7692ef2d2cf2e0116cca6cb8694c5379
Gustavo Pereira <gustavo.oito@hotmail.com>
2022-05-26 18:59:44 -0300

    Adição da tela do país e todos os seus cabeçalhos no arquivo de strings

---
+++ app/src/main/java/com/example/geosearch/countryPage.java
@@ -0,0 +1,14 @@
+package com.example.geosearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class countryPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_page);
+    }
+}
