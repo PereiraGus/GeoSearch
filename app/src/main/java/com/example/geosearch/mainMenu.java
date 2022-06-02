17636fc83197ce41b2c7d92e9ef91790ba71ff83
Gustavo Pereira <82532010+PereiraGus@users.noreply.github.com>
2022-06-02 10:35:13 -0300

    Atualizações do menu (instável)

--- app/src/main/java/com/example/geosearch/mainMenu.java
+++ app/src/main/java/com/example/geosearch/mainMenu.java
@@ -1,17 +1,27 @@
 package com.example.geosearch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class mainMenu extends AppCompatActivity {
+
+    private BottomNavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nav.setOnNavigationItemSelectedListener(R.id.menuBott);
+        nav.setSelectedItemId(R.id.bottomNavigationView);
     }
 }
