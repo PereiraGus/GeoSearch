dc19434e53033fa05e5fdeabf269edb745becf94
Gustavo Pereira <gustavo.oito@hotmail.com>
2022-06-03 20:05:46 -0300

    Implementação inicial do banco de dados

---
+++ app/src/main/java/com/example/geosearch/DatabaseHelper.java
@@ -0,0 +1,74 @@
+package com.example.geosearch;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "dbCountry.db";
+
+    public static final String COUNTRY_TABLE_NAME = "tbCountry";
+    public static final String COUNTRY_COLUMN_ID = "idCount";
+    public static final String COUNTRY_COLUMN_NAME = "nameCount";
+    public static final String COUNTRY_COLUMN_INIT = "init";
+    public static final String COUNTRY_COLUMN_CAPITAL = "capital";
+    public static final String COUNTRY_COLUMN_POP = "pop";
+    public static final String COUNTRY_COLUMN_DENSPOP = "dens";
+    public static final String COUNTRY_COLUMN_HDI = "hdi";
+    public static final String COUNTRY_COLUMN_RURALPOP = "ruralpop";
+    public static final String COUNTRY_COLUMN_URBANPOP = "urbanpop";
+    public static final String COUNTRY_COLUMN_LIFEEXPEC = "lifeexpec";
+    public static final String COUNTRY_COLUMN_TOTAREA = "totarea";
+    public static final String COUNTRY_COLUMN_BRUTEGDP = "brutegdp";
+    public static final String COUNTRY_COLUMN_CAPITAGDP = "capitagdp";
+    public static final String COUNTRY_COLUMN_HIST = "hist";
+
+    public static final String COUNTRY_COLUMN_CURRENCY = "crrc";
+    public static final String COUNTRY_COLUMN_REGION = "region";
+    public static final String COUNTRY_COLUMN_LANG = "lang";
+
+    public static final String USER_TABLE_NAME = "tbUser";
+    public static final String USER_COLUMN_ID = "idUser";
+    public static final String USER_COLUMN_NAME = "nameUser";
+    public static final String USER_COLUMN_COUNTRY = "countUser";
+    public static final String USER_COLUMN_PASSWORD = "passw";
+    public static final String USER_COLUMN_ISLOGGED = "islogged";
+
+    public static final String SAVECOUNTS_TABLE_NAME = "tbSaveCounts";
+    public static final String SAVECOUNTS_COLUMN_IDUSU = "idUser";
+    public static final String SAVECOUNTS_COLUMN_IDCOUNT = "idCount";
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(
+          "create table tbCountry" +
+            "(idCount integer primary key, nameCount text, init text, capital text, pop text," +
+                  "dens text, hdi text, ruralpop text, urbanpop text, lifeexpec text," +
+                  "totalarea text, brutegdp text, capitagdp text, hist text, crrc text," +
+                  "region text, lang text);");
+        db.execSQL(
+          "create table tbUser" +
+            "(idUser integer primary key, nameUser text, countUser text, passw text," +
+                  "islogged text);");
+        db.execSQL(
+          "create table tbSaveCounts" +
+                  "(idUser integer, idCount integer," +
+                  "FOREIGN KEY(idUser) references tbUser (idUser)," +
+                  "FOREIGN KEY(idCount) references tbCountry (idCount));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldDB, int newDB){
+        db.execSQL(
+                "drop table if exists tbCountry;");
+        db.execSQL(
+                "drop table if exists tbUser;");
+        db.execSQL(
+                "drop table if exists tbSaveCounts;");
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/geosearch/mainMenu.java
+++ app/src/main/java/com/example/geosearch/mainMenu.java
@@ -1,11 +1,15 @@
 package com.example.geosearch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class mainMenu extends AppCompatActivity {
 
@@ -13,5 +17,29 @@ public class mainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView nav = findViewById(R.id.bottomNav);
+        nav.setSelectedItemId(R.id.btnBarHome);
+        nav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.btnBarHome:
+                        Intent intentH = new Intent(getApplicationContext(), mainMenu.class);
+                        startActivity(intentH);
+                        return true;
+                    case R.id.btnBarMap:
+                        Intent intentM = new Intent(getApplicationContext(), selectionMap.class);
+                        startActivity(intentM);
+                        return true;
+                    case R.id.btnBarProf:
+                        Intent intentP = new Intent(getApplicationContext(), profileActivity.class);
+                        startActivity(intentP);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
